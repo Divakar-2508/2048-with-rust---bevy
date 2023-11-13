4bcd46131b1bc7762a76962aa65bbe29f168411b
Divakar-2508 <divakaraugust@gmail.com>
2023-11-13 15:04:48 +0530

    added font, system to update tile text

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,12 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, (setup, spawn_board))
+        .init_resource::<FontSpec>()
+        .add_systems(
+            Startup,
+            (setup, spawn_board, apply_deferred, spawn_tile).chain(),
+        )
+        .add_systems(Update, render_tile_points)
         .run()
 }
 
@@ -24,48 +29,6 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-#[derive(Component)]
-struct Points {
-    value: u32,
-}
-
-#[derive(Component)]
-struct Positon {
-    x: u8,
-    y: u8,
-}
-
-impl Positon {
-    fn new(x: u8, y: u8) -> Self {
-        Self { x, y }
-    }
-}
-
-#[derive(Component)]
-struct Board {
-    size: u8,
-    physical_size: f32,
-}
-
-impl Board {
-    fn new(size: u8) -> Self {
-        let physical_size = f32::from(size) * TILE_SIZE + f32::from(size + 1) * TILE_SPACER;
-        Self {
-            size,
-            physical_size,
-        }
-    }
-
-    fn cell_position_to_physical(&self, pos: u8) -> f32 {
-        let offset = -self.physical_size / 2.0 + 0.5 * TILE_SIZE;
-
-        offset + f32::from(pos) * TILE_SIZE + f32::from(pos + 1) * TILE_SPACER
-    }
-
-    fn size(&self) -> Vec2 {
-        Vec2::new(self.physical_size, self.physical_size)
-    }
-}
 fn spawn_board(mut commands: Commands) {
     let board = Board::new(4);
 
@@ -98,7 +61,7 @@ fn spawn_board(mut commands: Commands) {
         .insert(board);
 }
 
-fn spawn_tile(mut commands: Commands, query_board: Query<&Board>) {
+fn spawn_tile(mut commands: Commands, query_board: Query<&Board>, font_spec: Res<FontSpec>) {
     let board = query_board.single();
 
     let mut rng = rand::thread_rng();
@@ -107,24 +70,111 @@ fn spawn_tile(mut commands: Commands, query_board: Query<&Board>) {
         .cartesian_product(0..board.size)
         .choose_multiple(&mut rng, 2);
 
-    for(x, y) in starting_tiles.iter() {
+    for (x, y) in starting_tiles.iter() {
         let pos = Positon::new(*x, *y);
         commands
-            .spawn(
-                SpriteBundle {
-                    sprite: Sprite { 
-                        color: colors::TITLE_PLACEHOLDER,
-                        custom_size: Some(board.size()),
-                        ..default()
-                    },
-                    transform: Transform::from_xyz(
-                        board.cell_position_to_physical(pos.x), 
-                        board.cell_position_to_physical(pos.y), 
-                        1.0,
-                    ),
+            .spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: colors::TILE,
+                    custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
                     ..default()
+                },
+                transform: Transform::from_xyz(
+                    board.cell_position_to_physical(pos.x),
+                    board.cell_position_to_physical(pos.y),
+                    1.0,
+                ),
+                ..default()
             })
-            .insert(Points {value: 2})
+            .with_children(|builder| {
+                builder.spawn(Text2dBundle {
+                    text: Text::from_section(
+                        "4",
+                        TextStyle {
+                            font: font_spec.family.clone(),
+                            font_size: 40.0,
+                            color: Color::BLACK,
+                            ..default()
+                        },
+                    )
+                    .with_alignment(TextAlignment::Center),
+                    transform: Transform::from_xyz(0.0, 0.0, 1.0),
+                    ..default()
+                })
+                .insert(TileText);
+            })
+            .insert(Points { value: 2 })
             .insert(pos);
+    }
+}
+
+fn render_tile_points(mut texts: Query<&mut Text, With<TileText>>, tiles: Query<(&Points, &Children)>) {
+    
+    for (point, children) in tiles.iter() {
+        if let Some(entity) = children.first() {
+            let mut text = texts.get_mut(*entity).expect("No text component found!");
+            let mut text_section = text.sections.first_mut().expect("Can't get accessible text");
+            text_section.value = point.value.to_string();
+        }
+    }
+}
+
+#[derive(Component)]
+struct Points {
+    value: u32,
+}
+
+#[derive(Component)]
+struct TileText;
+
+#[derive(Component)]
+struct Positon {
+    x: u8,
+    y: u8,
+}
+
+#[derive(Resource)]
+struct FontSpec {
+    family: Handle<Font>,
+}
+
+impl FromWorld for FontSpec {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world
+            .get_resource_mut::<AssetServer>()
+            .unwrap();
+        FontSpec { family: asset_server.load("fonts/FiraSans-Bold.ttf") }
+    }
+}
+
+impl Positon {
+    fn new(x: u8, y: u8) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Component)]
+struct Board {
+    size: u8,
+    physical_size: f32,
+}
+
+impl Board {
+    fn new(size: u8) -> Self {
+        let physical_size = f32::from(size) * TILE_SIZE + f32::from(size + 1) * TILE_SPACER;
+        Self {
+            size,
+            physical_size,
+        }
+    }
+
+    fn cell_position_to_physical(&self, pos: u8) -> f32 {
+        let offset = -self.physical_size / 2.0 + 0.5 * TILE_SIZE;
+
+        offset + f32::from(pos) * TILE_SIZE + f32::from(pos + 1) * TILE_SPACER
+    }
+
+    fn size(&self) -> Vec2 {
+        Vec2::new(self.physical_size, self.physical_size)
     }
 }
